17882d82ed3b9292b0a3ee1f23fc9a00412d3155
HajarahM <108297190+HajarahM@users.noreply.github.com>
2022-09-06 19:39:39 +0100

    testing auto docker update

--- main.py
+++ main.py
@@ -390,6 +390,8 @@ class AWSConnect:
         inspect(engine).get_table_names()
        
 if __name__ == "__main__":
+    print('Starting...')
+
     with open ('credentials.json') as cred:
         credentials = json.load(cred)
     RDS_HOST = credentials['RDS_HOST']
